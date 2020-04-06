c072339865e176cbf8151ea70cbf60466ca66f43
Andrei Antal <antal.andrei@icloud.com>
2020-04-07 00:34:54 +0300

    Starting point

--- src/app/settings.service.ts
+++ src/app/settings.service.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SettingsService {
-  public readonly MAX_VOLUME = 300;
+  public readonly MAX_VOLUME = 200;
   public readonly STEP = 10;
   constructor(private http: HttpClient) {}
 }
